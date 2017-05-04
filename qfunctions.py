da962635664b8ec210f74280fbae8c4f4d763528
Toughee <MJ1menez@protonmail.com>
2017-05-03 21:35:12 -0800

    Put everything into a class object

--- qfunctions.py
+++ qfunctions.py
@@ -10,99 +10,106 @@
 # Vacation
 # Small sum purchase
 
-# Function decription : 
-Gross annual income per hour = {
-	
-	# Class
-	gross_income = input('enter your gross yearly income')
-	gross_hour_result = int(gross_income)
-	# fix final_income = gross_income // 52 // 40
-	Gross_Income_per_hour = print('Your income results to final_income as an hourly wage')
-}
+class inptfunction() :
+	# Function decription : Calculate how much you would earn hourly based on your annual gross income.
+	def gross_income():
+		
+		Gross annual income per hour = {
+			
+			# Class
+			gross_income = input('enter your gross yearly income')
+			gross_hour_result = int(gross_income)
+			# fix final_income = gross_income // 52 // 40
+			Gross_Income_per_hour = print('Your income results to final_income as an hourly wage')
+		}
 
-# Function decription : 
-Savings = {
+	# Function decription : Start your savings with this tool. 
+	def savings():
+	Savings = {
 
-	# Wealth
-	 Savings = input('Enter your savings')
-	 Earnings = input('Enter an approximate number of your earnings')
-	 Additional_earnings = input('Enter an estimate of the extra income you might earn')
-	 
-	# Time 
-	Years = //
-	Months = //
-	Weeks = //
-	Days = //
+		# Wealth
+		 Savings = input('Enter your savings')
+		 Earnings = input('Enter an approximate number of your earnings')
+		 Additional_earnings = input('Enter an estimate of the extra income you might earn')
+		 
+		# Time 
+		Years = //
+		Months = //
+		Weeks = //
+		Days = //
 
-	Time_for_savings = input('Enter the time you have left for saving money in in Weeks, Months, Years')
-	Desired_time_of use = input_int('Enter the time you wish to use it in Weeks, Months, Years')
+		Time_for_savings = input('Enter the time you have left for saving money in in Weeks, Months, Years')
+		Desired_time_of use = input_int('Enter the time you wish to use it in Weeks, Months, Years')
 
-	Savings_goal = input('Enter what you wish to save up')
-	Savings = print('Based on your current earnings, you have approximately #time ...
-	+....+... before you reach your goal of # for ...')
-}
+		Savings_goal = input('Enter what you wish to save up')
+		Savings = print('Based on your current earnings, you have approximately x amount of timetime ... +....+... before you reach your goal of  for ...')
+	}
 
-# Function decription : 
-College_fund_money = {
-	
-	# Important personal info
-	 Children = ('How many undergrads are going to attend College?')
-	 College = ('Where do they wish to go?')
+	# Function decription : Start your children's (or someone else's) future with this tool.
+	def College_fund_money(): 
+	College_fund_money = {
+		
+		# Important personal info
+		 Children = ('How many undergrads are going to attend College?')
+		 College = ('Where do they wish to go?')
 
-	# Wealth
-	 Savings = input('Enter your current savings')
-	 Earnings = input('Earnings')
-	 Additional_earnings = input('Enter an estimate of the extra income you might earn')
+		# Wealth
+		 Savings = input('Enter your current savings')
+		 Earnings = input('Earnings')
+		 Additional_earnings = input('Enter an estimate of the extra income you might earn')
 
-	# Time 
-	Years = //
-	Months = //
-	Weeks = //
-	Days = //
+		# Time 
+		Years = //
+		Months = //
+		Weeks = //
+		Days = //
 
-	Time_for_savings = input_int('Enter the time you have left for saving money in in Weeks, Months, Years')
-	Desired_time_of use = input_int('Enter the time you wish to use it in Weeks, Months, Years')
+		Time_for_savings = input_int('Enter the time you have left for saving money in in Weeks, Months, Years')
+		Desired_time_of use = input_int('Enter the time you wish to use it in Weeks, Months, Years')
 
-	Savings = print('Based on your current earnings, you have + + before you reach your goal of # for ')
-}
+		Savings = print('Based on your current earnings, you have + + before you reach your goal of # for ')
+	}
 
-# Function decription : 
-Vacation = {
-	Destination = ('Where do you wish to travel to?')
 
-	Savings = input('Enter your current savings')
-	Earnings = input('Enter an approximate number of your earnings')
-	Additional_earnings = input('Enter an estimate of the extra income you might earn')
-	
-	Time_for_savings = input('Enter the time you have left for saving money in in Weeks, Months, Years')
-	Desired_time_of use = input('Enter the time you wish to use it in Weeks, Months, Years')
+	# Function decription : Plan out your vacation with this tool. 
+	def Vacation(): 
+	Vacation = {
+		Destination = ('Where do you wish to travel to?')
 
-	Misc_purchases = input('Enter the amount you wish to spend on small things')
+		Savings = input('Enter your current savings')
+		Earnings = input('Enter an approximate number of your earnings')
+		Additional_earnings = input('Enter an estimate of the extra income you might earn')
+		
+		Time_for_savings = input('Enter the time you have left for saving money in in Weeks, Months, Years')
+		Desired_time_of use = input('Enter the time you wish to use it in Weeks, Months, Years')
 
-	Vacation = print('Based on your current earnings, you have .... ...+...+... before you reach your goal of # for ...')
-}
+		Misc_purchases = input('Enter the amount you wish to spend on small things')
 
-# Function decription : 
-Small_sum_purchase = {
-	
-	# Class
-	Purchase = input('what do you want to buy?')
-	
-	# Wealth
-	Savings = input('')
-	Earnings = input('Enter an approximate number of your earnings')
-	Additional_earnings = input('Enter an estimate of the extra income you might earn')
-	
-	# Time 
-	Years = //
-	Months = //
-	Weeks = //
-	Days = //
+		Vacation = print('Based on your current earnings, you have .... ...+...+... before you reach your goal of # for ...')
+	}
 
-	Time_for_savings = input('Enter the time you have left for saving money in in Weeks, Months, Years')
-	Desired_time_of_use = input('')
-	
-	Misc_purchases = input('Enter the amount you wish to spend on small things')
-	
-	Small_sum_purchase = print('Based on your current earnings, you have approximately ...  to buy  .... with ...+...+... before you reach your goal of # for ...')
-}
+	# Function decription : Tool for planning out a moderately expensive purchase
+	def Small_sum_purchase():
+	Small_sum_purchase = {
+		
+		# Class
+		Purchase = input('what do you want to buy?')
+		
+		# Wealth
+		Savings = input('')
+		Earnings = input('Enter an approximate number of your earnings')
+		Additional_earnings = input('Enter an estimate of the extra income you might earn')
+		
+		# Time 
+		Years = //
+		Months = //
+		Weeks = //
+		Days = //
+
+		Time_for_savings = input('Enter the time you have left for saving money in in Weeks, Months, Years')
+		Desired_time_of_use = input('')
+		
+		Misc_purchases = input('Enter the amount you wish to spend on small things')
+		
+		Small_sum_purchase = print('Based on your current earnings, you have approximately ...  to buy  .... with ...+...+... before you reach your goal of # for ...')
+	}
