d02090b1e580cae84a846e0e4af969c9708f9723
Toughee <MJ1menez@protonmail.com>
2017-07-15 20:51:57 -0700

    Fixed javascript for web app
    Everything shows up as needed

--- js/Calculate.js
+++ js/Calculate.js
@@ -1,83 +1,33 @@
-function budget(tags)
-
-	// Show splash page and prompt
-	// same user prompt as the python script
-	// output what the user inputs in data
-		// return calculate
-	// Show on the screen
-
-var gross_income() {
-	// Function decription : Calculate how much you would earn hourly based on your annual gross income.
-	var gross_income;
-		var gross_income = input(self, text='enter your gross yearly income')
-			var gross_hour_result = int(gross_income)
-			var final_income = gross_income // 52 // 40
-			var Gross_Income_per_hour = (self, text='Your income results to final_income as an hourly wage')
-}
-// Start your savings with this tool. 
-  var savings() {
-		/* Wealth */
-		 var Savings = input(self, text='Enter your savings')
-		 var Earnings = input(self, text='Enter an approximate number of your earnings')
-		 var Additional_earnings = input(self, text='Enter an estimate of the extra income you might earn')
-		/* Time */ 
-		var Years = //
-		var Months = //
-		var Weeks = //
-		var Days = //
-		var Time_for_savings = input(self, text='Enter the time you have left for saving money in in Weeks, Months, Years')
-		var Desired_time_of_use = input_int(self, text='Enter the time you wish to use it in Weeks, Months, Years')
-		var Savings_goal = input(self, text='Enter what you wish to save up')
-		var Savings = print(self, text='Based on your current earnings, you have approximately x amount of time before you reach your goal of for ...')
-}
-// Function decription : Start your children's (or someone else's) future with this tool.
-var College_fund_money(){ 
-  // Important personal info
-		 var Children = (self, text='How many undergrads are going to attend College?')
-		 var College = (self, text='Where do they wish to go?')
-     var Semester = 
-	// Wealth
-		 var Savings = input(self, text='Enter your current savings')
-		 var Earnings = input(self, text='Earnings')
-		 var Additional_earnings = input(self, text='Enter an estimate of the extra income you might earn')
-	// Time 
-		var Years = (self, text='')
-		var Months = (self, text='')
-		var Weeks = (self, text='')
-		var Days = (self, text='')
-	  var Time_for_savings = input_int('Enter the time you have left for saving money in in Weeks, Months, Years')
-		self.Desired_time_of use = input_int('Enter the time you wish to use it in Weeks, Months, Years')
-		self.Savings = print('Based on your current earnings, you have + + before you reach your goal of # for ')
-      return ();
-}
-	// Function decription : Plan out your vacation with this tool. 
-	function Vacation() { 
-		var Destination = (self, text='Where do you wish to travel to?')
-		var Savings = (self, text='Enter your current savings')
-		var Earnings = (self, text='Enter an approximate number of your earnings')
-		var Additional_earnings = (self, text='Enter an estimate of the extra income you might earn')
-		var Time_for_savings = (self, text='Enter the time you have left for saving money in in Weeks, Months, Years')
-		var Desired_time_of use = (self, text='Enter the time you wish to use it in Weeks, Months, Years')
-		var Misc_purchases = (self, text='Enter the amount you wish to spend on small things')
-		var Vacation = (self, text='Based on your current earnings, you have .... ...+...+... before you reach your goal of # for ...')
-}
-	// Function decription : Tool for planning out a moderately expensive purchase
-	function Small_sum_purchase() {
-		// Class
-		var Purchase = (self, text='what do you want to buy?')
-		// Wealth
-		var Savings = (self, text='')
-		var Earnings = (self, text='Enter an approximate number of your earnings')
-		var Additional_earnings = (self, text='Enter an estimate of the extra income you might earn')
-		// Time 
-		var Years = //
-		var Months = //
-		var Weeks = //
-		var Days = //
-		var Time_for_savings = input(self, text='Enter the time you have left for saving money in in Weeks, Months, Years')
-		var Desired_time_of_use = input('')
-		var Misc_purchases = input(self, text='Enter the amount you wish to spend on small things')
-		var Small_sum_purchase = (self, text='Based on your current earnings, you have approximately ...  to buy  .... with ...+...+... before you reach your goal of # for ...')
-}
-if __name__ == "__main__":
-	mainloop()
+function calculate1() {
+        var a = document.getElementById("qty1-class1").value  / 52 / 40;
+            document.getElementById("num1").innerHTML = 'Your total gross pay rate per hour equals to '+ a;
+    }
+    function calculate2() {
+        var bb = parseInt(document.getElementById("qty1-class2").value);
+        var bc = parseInt(document.getElementById("qty2-class2").value);
+        var bd = parseInt(document.getElementById("qty3-class2").value);
+        var be = parseInt(document.getElementById("qty4-class2").value);
+        var bf = bb + bc + bd;
+        var bg = be - bf;
+            document.getElementById("num2").innerHTML = 'You only need  ' + bg + '  before you reach your goal of ' + be;
+    }
+    function calculate3() {
+        var c1 = document.getElementById("collegeName").value;
+        var c2 = parseInt(document.getElementById("tuit").value);
+        var c3 = parseInt(document.getElementById("semester").value);
+        var c4 = parseInt(document.getElementById("qty5-class3").value);
+        var c5 = parseInt(document.getElementById("qty6-class3").value);
+        var c6 = parseInt(document.getElementById("qty7-class3").value);
+        var c7 = document.getElementById("qty8-class3").value;
+     	var c8 = c2 + c4 + c5 + c6 * c3;
+            document.getElementById("num3").innerHTML = 'Your total cost for attending ' + c1 + ' would total into ' + c8 + ' if you wish to attend on ' + c7;
+    }
+    function calculate4() {
+        var d1 = document.getElementById("destination").value;
+        var d2 = parseInt(document.getElementById("qty1-class4").value);
+        var d3 = parseInt(document.getElementById("qty2-class4").value);
+        var d4 = parseInt(document.getElementById("travelFare").value);
+        var d7 = parseInt(document.getElementById("qty6-class4").value);
+        var d8 = d4 + d7;
+            document.getElementById("num4").innerHTML = 'Your total cost for visiting The ' + d1 + ' would be ' + d8;
+    }
