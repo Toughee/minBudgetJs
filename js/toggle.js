55f5d4a14459d80953c5fe01e7b1306f17a0c344
Toughee <MJ1menez@protonmail.com>
2017-07-15 20:53:45 -0700

    Added javascript for displaying budget options
    A few details need to be finished

---
+++ js/toggle.js
@@ -0,0 +1,36 @@
+
+function btn1() {
+    var x = document.getElementById('grossIncomeSect');
+        if (x.style.display === 'none') {
+            x.style.display = 'block';
+        } else {
+            x.style.display = 'none';
+        }
+}
+
+function btn2() {
+    var x = document.getElementById('savingsSect');
+        if (x.style.display === 'none') {
+            x.style.display = 'block';
+        } else {
+            x.style.display = 'none';
+        }
+}
+
+function btn3() {
+    var x = document.getElementById('collegeFundSect');
+        if (x.style.display === 'none') {
+            x.style.display = 'block';
+        } else {
+            x.style.display = 'none';
+        }
+}
+
+function btn4() {
+    var x = document.getElementById('vacationSect');
+        if (x.style.display === 'none') {
+            x.style.display = 'block';
+        } else {
+            x.style.display = 'none';
+        }
+}
